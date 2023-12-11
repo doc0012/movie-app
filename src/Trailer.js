81fbb42be8cfb06a09b92344e8ad581d44bfd888
MOHAMED RIAHI <mohariahi@yahoo.com>
2023-12-11 12:13:22 +0100

    update

--- src/Trailer.js
+++ src/Trailer.js
@@ -14,11 +14,17 @@ navigate("/")
         <>
       
 <iframe width="853" height="480" src={movie.trailer} title="Tesla Cybertruck DRAG RACE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        
+
+<p style={{ fontSize: '14px', color: 'white' }}>{movie.description}</p>
+       
         <button className="Add-btn" onClick={redirection} >Home</button>
 
         </>
     )
 }
+
+
+    
+
 export default Trailer
 
